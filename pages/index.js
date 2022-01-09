f007011f7bd6839112ce9fefa7dfb3de937f9f6c
Pietro Di Chio <pietro.dichio@talentgarden.com>
2022-01-09 10:48:54 +0100

    add validation through yup and error messages

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,45 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import moment from 'moment';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
+import * as yup from 'yup';
 
 export default function Home() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const schema = yup
+    .object({
+      name: yup.string().required('Company name is a required field'),
+      startDate: yup
+        .string()
+        .required('Deve esserci una data di inizio')
+        .matches(
+          /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/,
+          'La data deve essere nel formato dd/mm/yyyy'
+        ),
+      endDate: yup
+        .string()
+        .required('Deve esserci una data di fine')
+        .matches(
+          /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/,
+          'La data deve essere nel formato dd/mm/yyyy'
+        ),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, values },
+  } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  });
   const [startDate, setStartDate] = useState(
     moment().subtract(1, 'month').format('DD/MM/YYYY').toString()
   );
   const [endDate, setEndDate] = useState(moment().format('DD/MM/YYYY').toString());
+  const onSubmit = (data) => console.log(data);
   return (
     <div className={styles.container}>
       <Head>
@@ -37,23 +66,27 @@ export default function Home() {
                 <label>Data iniziale</label>
                 <input
                   name="datepicker"
-                  datepicker
                   type="text"
                   className="bg-gray-50 border  sm:text-sm rounded-lg  block w-full pl-2.5 p-2.5 border-gray-600 placeholder-gray-400 text-black focus:ring-blue-500 focus:border-blue-500"
                   placeholder={startDate}
-                  {...register('startDate', { required: true, maxLength: 10, minLength: 10 })}
+                  {...register('startDate')}
                 />
+                {errors?.endDate?.message && (
+                  <p className="text-sm text-red-600">{errors?.endDate?.message}</p>
+                )}
               </div>
               <div className="flex flex-col gap-5">
                 <label>Data finale</label>
                 <input
                   name="datepicker"
-                  datepicker
                   type="text"
                   className="bg-gray-50 border  sm:text-sm rounded-lg  block w-full pl-2.5 p-2.5 border-gray-600 placeholder-gray-400 text-black focus:ring-blue-500 focus:border-blue-500"
                   placeholder={endDate}
                   {...register('endDate')}
                 />
+                {errors?.endDate?.message && (
+                  <p className="text-sm text-red-600">{errors?.endDate?.message}</p>
+                )}
               </div>
             </div>
             <input
